74281b19bca89e6b17abbd5e6b34e5347e554b78
Ashwin Gatadi <ashwin.gatadi@gmail.com>
2017-06-01 17:21:57 -0700

    init commit

---
+++ DependencyInversion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles
+{
+    public class DependencyInversion
+    {
+        //High level modules should not depend on low level modules. 
+        //High level and low level modules should depend on abstraction. 
+    }
+}
---
+++ LiskovSubstitution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles
+{
+    public class LiskovSubstitution
+    {
+        //if we replace base type with the child type then the behaviour should not change 
+    }
+}
